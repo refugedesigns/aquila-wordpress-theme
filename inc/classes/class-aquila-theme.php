049d4307218bd5c194d024024c720ff740b89b13
Erasmus <refugedesigns17@gmail.com>
2023-03-02 15:50:54 +0000

    added theme support for title-tag, custom log and custom background

--- inc/classes/class-aquila-theme.php
+++ inc/classes/class-aquila-theme.php
@@ -14,8 +14,28 @@
     
     protected function __construct() {
         // load classes
-        Assets::get_instance();     
+        Assets::get_instance();
+        $this->setup_hooks();     
     }
     
-  
+    public function setup_hooks() {
+        add_action('after_setup_theme',  [$this, 'theme_setup']);
+    }
+
+    public function theme_setup() {
+        add_theme_support('title-tag');
+
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true,
+        ]);
+
+        add_theme_support('custom-background', [
+            'background-color' => '#ffffff',
+            'default-image' => '',
+        ]);
+    }
  }
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -7,7 +7,13 @@
 ?>
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#"><?php
+         if(function_exists('the_custom_logo')) {
+            the_custom_logo();
+         } else {
+            echo 'Navbar';
+         }
+         ?></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
