2c4d1180918ad9dbf61997c6b0c4a7b2565a8a59
Erasmus <refugedesigns17@gmail.com>
2023-03-06 07:01:04 +0000

    displayed featured images, added custom image sizes functionality, added custome meta-boxes, added nounce verification to hide_title_meta_box

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -1,23 +1,29 @@
 <?php
 
-function get_the_post_custom_thumbnail($post_id, $size="featured_large", $additional_attributes=[]) {
+function get_the_post_custom_thumbnail($post_id, $size="featured_thumbnail", $additional_attributes=[]) {
     if($post_id === null) {
         $post_id = get_the_ID();
     }
 
     if(has_post_thumbnail($post_id)) {
+
+        $default_attributes = [
+            'loading' => 'lazy',
+        ];
+
+        $attributes = array_merge($default_attributes, $additional_attributes);
         
         $custom_thumbnail = wp_get_attachment_image(
             get_post_thumbnail_id($post_id),
             $size,
             false,
-            $additional_attributes
+            $attributes
         );
     }
 
     return $custom_thumbnail;
 }
 
-function the_post_custom_thumbnail($post_id, $size="featured_large", $additional_attributes=[]) {
+function the_post_custom_thumbnail($post_id, $size="featured_thumbnail", $additional_attributes=[]) {
     echo get_the_post_thumbnail($post_id, $size, $additional_attributes);
 }
